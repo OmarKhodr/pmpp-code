8679aae0ab3ce09d1d74ecc10431c317fd41f1dc
Omar Khodr <omar.khoder2000@gmail.com>
2025-11-28 21:01:13 +0100

    Fix typo

--- cuda/vec-add/main.cu
+++ cuda/vec-add/main.cu
@@ -33,7 +33,7 @@ void vecAdd(float *A_h, float *B_h, float *C_h, int n) {
 }
 
 int main(int argc, const char *argv[]) {
-	cudaDeviceSynchorize();
+	cudaDeviceSynchronize();
 
 	printf("Initializing...\n");
 
